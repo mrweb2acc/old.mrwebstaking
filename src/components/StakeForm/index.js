f4131ff3fdcec44e65a0eed3572814fc5ea3330b
mrweb2acc <84605472+mrweb2acc@users.noreply.github.com>
2021-06-13 06:45:05 +0000

    Update index.js

--- src/components/StakeForm/index.js
+++ src/components/StakeForm/index.js
@@ -42,5 +42,97 @@ export default function StakeForm({ contract, tokenContract, address }) {
     setApyModel("30");
   };
 
+  React.useEffect(() => {
+    contract
+      .users(address)
+      .call()
+      .then((data) => {
+        window.tronWeb.toDecimal(data.finalCompoundAsset) !== 0 &&
+          setIsLocked(true);
+
+        setStakedAmount(
+          window.tronWeb.toDecimal(data.initialInvestment / 1000000)
+        );
+
+        const timestamp = window.tronWeb.toDecimal(data.investmentPeriodEndsAt);
+
+        setStakePeriodStakeEndsAt(
+          (timestamp - Math.floor(new Date().getTime() / 1000.0)) / 86400
+        );
+      });
+
+    contract
+      .currentPoolAmount()
+      .call()
+      .then((data) => {
+        console.log(window.tronWeb.toDecimal(data))
+        if (window.tronWeb.toDecimal(data) >= 100000000) {
+          setIsPoolPacked(true);
+        }
+      });
+  }, [address, contract]);
+  return (
+    <div className="stakeForm">
+      {/* form title */}
+      <div className="title">
+        <div className="apyModelContainer">
+          <AccountBalanceWalletIcon />
+          <div className="content">
+            APY: 30 Days = 7%, 60 Days = 17%, 90 Days = 36%
+          </div>
+        </div>
+
+        <div className="stakedAmountContainer">Staked: {stakedAmount} AMA</div>
+      </div>
+
+      {/* form input */}
+      {
+        isPoolPacked && 
+        <h2 style={{marginTop: "30px"}}>
+          The Stake Pool is currently full!
+        </h2>
+      }
+      {/* pool is not packed */}
+      {!isPoolPacked && !isLocked && (
+        <div className="inputContainer">
+          <div className="input">
+            <label>Amount: Minimum 100000000000</label>
+            <input
+              type="text"
+              placeholder="0"
+              value={amount}
+              onChange={(e) => setNewAmount(e.target.value)}
+            />
+            <span>AMA</span>
+          </div>
+
+          <div className="apySelection" style={{ marginTop: "15px" }}>
+            <span>Select APY Model&nbsp;</span>
+            <select
+              value={apyModel}
+              onChange={(e) => setApyModel(e.target.value)}
+            >
+              <option value="30">30 Days</option>
+              <option value="60">60 Days</option>
+              <option value="90">90 Days</option>
+            </select>
+          </div>
+          <Button onClick={submitStake}>Approve</Button>
+        </div>
+      )}
+
+      {/* is staked already */}
+      {isLocked && (
+        <div className="stakePeriodExist">
+          <p>Your stake period has not ended yet.</p>
+          <p>
+            {(stakePeriodEndsAt * 24 * 60).toFixed(3) < 0
+              ? "0"
+              : (stakePeriodEndsAt * 24 * 60).toFixed(3)}{" "}
+            mins left
+          </p>
+        </div>
+      )}
+    </div>
   );
 }
