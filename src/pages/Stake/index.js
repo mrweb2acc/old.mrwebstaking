e31d56c760a363af76063374c4c4e7406c9a67aa
mrweb2acc <84605472+mrweb2acc@users.noreply.github.com>
2021-06-13 06:58:02 +0000

    Update index.js

--- src/pages/Stake/index.js
+++ src/pages/Stake/index.js
@@ -99,10 +99,10 @@ export default function Stake() {
                 address={address}
               />
 
-              <div className="dividendContainer">
-                <DividendContainer
-                  contract={contract}
-                  tokenContract={tokenContract}
+           ##   <div className="dividendContainer">
+          ##      <DividendContainer
+          ##        contract={contract}
+           ##       tokenContract={tokenContract}
                 />
               </div>
             </div>
